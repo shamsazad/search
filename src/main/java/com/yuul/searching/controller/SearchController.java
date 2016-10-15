f8cbcfb0a10e388dc7cd4cb7fee27ac41ec7cb7b
i848920 <shams.abubakar.azad@sap.com>
2016-10-15 17:23:24 -0400

    Added enable CrossOrigin

--- src/main/java/com/yuul/searching/controller/SearchController.java
+++ src/main/java/com/yuul/searching/controller/SearchController.java
@@ -4,10 +4,7 @@ import com.yuul.searching.Model.Search;
 import com.yuul.searching.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.persistence.Entity;
 
 @RestController
 @RequestMapping("/yuul/search/")
@@ -18,6 +15,7 @@ public class SearchController {
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @ResponseBody
+    @CrossOrigin
     public Search get(@PathVariable int id) {
 
         return searchService.findOne(id);
@@ -32,6 +30,7 @@ public class SearchController {
 
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
+    @CrossOrigin
     public Iterable<Search> getAll() {
         return searchService.findAll();
     }
