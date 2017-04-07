1c3d97ff203a720d32eba2be3e6a105c50b54899
Badal Desai <badal.desai@hmhco.com>
2017-04-07 15:41:42 -0400

    Added the spring boot test

--- src/test/java/com/yuul/searching/controller/SearchControllerTest.java
+++ src/test/java/com/yuul/searching/controller/SearchControllerTest.java
@@ -1,5 +1,7 @@
 package com.yuul.searching.controller;
-
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import com.yuul.searching.Model.Search;
 import com.yuul.searching.repository.ISearchRepository;
 import org.junit.Before;
@@ -7,10 +9,15 @@ import org.junit.Test;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class SearchControllerTest {
 
+    @Autowired
     private SearchController service;
+    @Autowired
     private Search result;
     @Autowired
     private ISearchRepository searchRepository;
@@ -26,7 +33,7 @@ public class SearchControllerTest {
     @Test
     public void shouldGetService(){
         this.result = this.service.get(1);
-        assertThat(result,equalTo(this.searchRepository.findOne(1)));
+        assertThat(result,equalTo(searchRepository.findOne(1)));
     }
 
 }
